7a5637763fb3656d1db8e309c0682cc668b91be8
Mike MacAdam <mikemacadam87@gmail.com>
2017-06-15 12:08:44 -0700

    cp2 with assignment

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -1,9 +1,22 @@
 require "bloc_works/version"
+require "bloc_works/controller"
+require "bloc_works/utility"
+require "bloc_works/router"
+require "bloc_works/dependencies"
 
 module BlocWorks
   class Application
-  def call(env)
-    [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads!"]]
+    def call(env)
+      response = self.root(env)
+
+      if response
+        return response
+      else
+        cont_array = self.controller_and_action(env)
+        cont = cont_array.first.new(env)
+        action_call = cont.send(cont_array.last)
+        return [200, {'Content-Type' => 'text/html'}, [action_call]]
+      end
+    end
   end
-end
 end
